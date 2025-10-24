7a337894ba9526f285f501ce3da21ca834672414
Babandeep Singh <babandeepsingh@gmail.com>
2025-10-25 01:52:53 +0530

    Remove secrets, use environment variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,13 +12,17 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: [
+   ignores: [
       "node_modules/**",
       ".next/**",
       "out/**",
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",      // allow 'any'
+      "@typescript-eslint/no-unused-vars": "warn",      // allow unused variables
+    },
   },
 ];
 
